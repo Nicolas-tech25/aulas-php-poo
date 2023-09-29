2c1deb4fc44ba25aa885b51a680bc804de595ca4
Nicolas_Amorim <ni.oliveiraamorim.25@gmail.com>
2023-09-29 08:47:50 -0300

    métodos estáticos
    métodos estáticos  com o mesmo nome = erro :(

--- index.php
+++ index.php
@@ -3,49 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo 08</title>
+    <title>Exemplo 09</title>
 </head>
 <body>
 
-    <h1>PHP com POO - Exemplo 08</h1>
+    <h1>PHP com POO - Exemplo 09</h1>
     <hr> 
 
     <h2>Assuntos Abordados: </h2>
 
     <ul>
-        <li>Propriedades e métodos estáticos</li>
-        <li>Acesso direto sem objetos/instâncias</li>
-        <li>uso do <code>self</code> para acessar dentro da classe os recursos estáticos</li>
+        <li>Namespaces: agrupamento e organização de recursos (classes,funções,constantes)</li>
+        <li>Prevenção de conflitos entre classes de mesmo nome</li>
+        <li>Configurar e usar <code>namespace</code> e <code>alias</code></li>
     </ul>
 
     <hr>
 
     <?php
-        require_once "src/pessoaFisica.php";
-        $Cliente1 = new pessoaFisica;
-        $Cliente1->setNome("Astrogildo");
-        $Cliente1->setIdade(75);
-
-        $Cliente2 = new pessoaFisica;
-        $Cliente2->setNome("Jorge");
-        $Cliente2->setIdade(18);
-
-        require_once "src/utilitarios.php";
-        Utilitarios::obterData();
+        /* Só de fazer o require/importação das classes, já da erro no servidor devido a terem o mesmo nome. */
+        require_once "src/fornecedores/Pagamento.php";
+        require_once "src/prestadores/Pagamento.php";
     ?>
-
-    <h2>Atendimentos do dia: <?=Utilitarios::$dataAtual?></h2>
-
-    <h3>Cliente: <?=$Cliente1->getNome()?></h3>
-    <p>Tipo de atendimento:
-        <?=Utilitarios::definirAtendimento($Cliente1->getIdade())?>
-    </p>
-
-    <h3>Cliente: <?=$Cliente2->getNome()?></h3>
-    <p>Tipo de atendimento:
-        <?=Utilitarios::definirAtendimento($Cliente2->getIdade())?>
-    </p>
-
-    
 </body>
 </html>
